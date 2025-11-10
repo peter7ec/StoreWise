7c2e0176e8a4f00535dd656cc6e6fd68b6161033
Garai Péter <peter7ec@gmail.com>
2025-11-10 11:42:47 +0100

    Edit hompage and add texts

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,22 @@
-import { Box } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 
 export default function HomePage() {
-  return <Box sx={{ textAlign: "center" }}>Üdvözöljük a StoreWise-on!</Box>;
+  return (
+    <Box sx={{ textAlign: "center" }}>
+      <Typography
+        variant="h1"
+        sx={{ mb: 5, color: "#2196f3", fontSize: { xs: 35, xl: 50 } }}
+      >
+        Üdvözöljük a StoreWise-on!
+      </Typography>
+      <Container
+        sx={{ bgcolor: "#2196f3", color: "white", borderRadius: 2, p: 2 }}
+      >
+        <Typography variant="body1">
+          Egy saját bevásárló lista vezető oldal ahol te mondod meg, hogy mit
+          honnan szeretnél megvásárolni és ezt egy átlátható listába láthatod!
+        </Typography>
+      </Container>
+    </Box>
+  );
 }
